cb4ad91440128e378c2c1c85f554b10f48945172
brovins4723 <brovins4723@gmail.com>
2025-11-02 11:41:42 -0500

    Update main.js

--- Prototyping/main.js
+++ Prototyping/main.js
@@ -6,13 +6,6 @@ const ctx = new AudioContext();
 const master = new GainNode(ctx);
 master.connect(ctx.destination);
 
-const delayGain = new GainNode(ctx);    // separate gain node for the delayed signal
-const delay = new DelayNode(ctx, {
-  delayTime: 0.0,
-  maxDelayTime: 5.0
-});
-delay.connect(delayGain).connect(ctx.destination);
-
 const myNotes = new Array(128);         // new notes are stored in an array where index = the midi note#
 const adsr = [0.2, 0.15, 0.4, 0.5];     // storing ADSR values in a new array
         // envelope attack time (sec)
@@ -26,7 +19,6 @@ const midi = new MIDIengine();
 midi.onNoteOn = (note, velocity) => {
     myNotes[note] = new Synth(ctx, note, velocity, adsr);   // passing note and velocity as parameters for a new synth note... also using the ADSR array as a parameter
     myNotes[note].env.connect(master);      // connect the envelope (gain node of Synth) to the master, as well as the delay node
-    myNotes[note].env.connect(delay);
     myNotes[note].start(note, velocity);
     console.log("start");
 }
@@ -63,17 +55,32 @@ document.querySelector("#releaseSlider").addEventListener("input", (event)=>{
 })
 
 
-// --- --- --- also sliders for delay time and delay gain, updates the values for delayTime and delay.gain
-
-document.querySelector("#delayTime").addEventListener("input", (event)=>{
-    document.querySelector("#delayTimeValue").textContent = `${event.target.value} sec`
-    delay.delayTime.value = Number(event.target.value);
-})
-document.querySelector("#delayGain").addEventListener("input", (event)=>{
-    document.querySelector("#delayGainValue").textContent = `${event.target.value}`
-    delayGain.gain.value = Number(event.target.value);
-})
+// --- --- --- also master gain slider
 document.querySelector("#masterGain").addEventListener("input", (event)=>{
     document.querySelector("#masterGainValue").textContent = `${event.target.value}`
     master.gain.value = Number(event.target.value);
 })
+
+// --- --- --- articulation selector buttonz
+const artic1 = document.getElementById('artic1');
+const artic2 = document.getElementById('artic2');
+const artic3 = document.getElementById('artic3');
+const artic4 = document.getElementById('artic4');
+
+
+// Add individual listeners
+artic1.addEventListener('click', () => {
+  console.log('The notes will be slurred !');
+});
+
+artic2.addEventListener('click', () => {
+  console.log('The notes will be long and accented');
+});
+
+artic3.addEventListener('click', () => {
+  console.log('The notes will be short and stopped');
+});
+
+artic4.addEventListener('click', () => {
+  console.log('The notes will be short and off-the-string');
+});
