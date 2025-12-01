d6f6133a5f378059d7beb173c7600c42da196844
brovins4723 <brovins4723@gmail.com>
2025-12-01 12:19:04 -0500

    Update synth.js

--- Prototyping/synth.js
+++ Prototyping/synth.js
@@ -8,34 +8,36 @@ export default class Synth {
         this.adsr = adsr;
         this.filterEnv = filterEnv;
 
-        // --- --- --- SWITCHTED TO ALL TONE.JS AUDIO NODES --- --- ---
-
-        // this.osc = new OscillatorNode(this.ctx);                // new oscillator node for each new Synth
-        // this.osc.frequency.value = this.mtof(this.midiNote);    // converting midi note input to frequency
-        // this.osc.type = "sawtooth";
-
-        // this.ampEnv = new GainNode(this.ctx);
         this.maxGain = 0.2;      // maximum loudness (one note)
-        // this.filter = new BiquadFilterNode(this.ctx);
-        // this.filter.type = "lowpass";
-        // this.filter.frequency.value = 2000;
+
+        // --- --- --- SWITCHTED TO ALL TONE.JS AUDIO NODES --- --- ---
+   
         // --- --- ---
 
         Tone.setContext(this.ctx);
+        this.freq = this.mtof(this.midiNote)
 
-        this.osc = new Tone.Oscillator(this.mtof(this.midiNote), "sawtooth");
-        this.osc1 = new Tone.Oscillator(this.mtof(this.midiNote+0.1), "sawtooth");
-        this.osc2 = new Tone.Oscillator(this.mtof(this.midiNote-0.1), "sawtooth");
-        this.osc3 = new Tone.Oscillator(this.mtof(this.midiNote-0.3), "sawtooth");
+        this.osc = new Tone.Oscillator(this.freq, "sawtooth");
+        // this.osc1 = new Tone.Oscillator(this.mtof(this.midiNote+0.1), "sawtooth");
+        // this.osc2 = new Tone.Oscillator(this.mtof(this.midiNote-0.1), "sawtooth");
+        // this.osc3 = new Tone.Oscillator(this.mtof(this.midiNote-0.3), "sawtooth");
+
+        this.vibLFO = new Tone.LFO({
+            frequency: 4,     // vibrato rate
+            amplitude: 15,    // vibrato width (in cents)(10-25cents)
+        });
+        // Connect LFO to the oscillator freq value
+        this.vibLFO.connect(this.osc.detune);
+
+        // Start LFO immediately (does it need to happen in the start method?)(no! it should be running continuously!)
+        this.vibLFO.start();
 
         this.convolver = new Tone.Convolver("IR files/celloIR(cello3_eqed_dc).wav"); // new convolver node with IR file inside buffer
         this.convolver.wet = 1.;
         this.filter = new Tone.Filter(2000, "lowpass");
         this.ampEnv = new GainNode(this.ctx);   // vanilla javascript audio node
 
-        //this.osc.connect(this.filter).connect(this.ampEnv);
-       this.osc.connect(this.convolver).connect(this.filter);
-       //this.osc.connect(this.filter);
+        this.osc.connect(this.convolver).connect(this.filter);
         Tone.connect(this.filter, this.ampEnv);
 
     }
@@ -92,4 +94,4 @@ export default class Synth {
         //stop the oscillator
         this.osc.stop(now + this.adsr[this.adsr.length-1][1]);
     }
-}
+};
