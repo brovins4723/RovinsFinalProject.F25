0bb7c48b170352f27ed1936a4cb928486d62dbc0
brovins4723 <brovins4723@gmail.com>
2025-12-08 10:51:03 -0500

    Update main.js

--- Prototyping/main.js
+++ Prototyping/main.js
@@ -6,6 +6,13 @@ const ctx = new AudioContext();
 const master = new GainNode(ctx)
 master.gain.value = 0.5;
 master.connect(ctx.destination);
+const lpFilter = new BiquadFilterNode(ctx, {
+    type: "highshelf",
+    frequency: 3300,   // center frequency of the shelf
+    gain: 0            // start flat
+});
+lpFilter.connect(master);
+
 
 const myNotes = new Array(128);         // new notes are stored in an array where index = the midi note#
 
@@ -13,7 +20,7 @@ const value = null
 const duration = null
 let articNum = 0;
 let dynamicNum = 1;
-let vibratoAmount = 0;
+
 const envArray = [     // array of all amplitude envelopes
   // each array = [amplitude, filter1 boost, filter2 boost, duration]
   // [                     // FIRST DRAFT
@@ -22,42 +29,42 @@ const envArray = [     // array of all amplitude envelopes
   // [0, 800, 3000, 0.4],
   // ],
   [                     // amp envelope 1 ; LEGATO
-  [0.00,   0,   0,    0.00],   // start
-  [0.80,   5,   3,    0.12],   // slow bow-attack
-  [0.65,   3,   2,    0.10],   // slight decay
-  [0.60,   2,   1,    0.40],   // sustain plateau
-  [0.00,   0,   0,    0.30]    // soft release
+  [0.00,   6,    12,  0.00],
+  [0.80,   12,   12,  0.20],   // slow bow-attack
+  [0.65,   6,   10,   0.10],   // slight decay
+  [0.60,   6,   9,    0.40],   // sustain plateau
+  [0.00,   6,   9,    0.20]    // soft release
   ],
   [                     // amp envelope 2 ; STACCATO
-  [0.00,   0,    0,     0.00],
-  [0.90,   7,    4,     0.02],  // bite
-  [0.40,   4,    2,     0.04],  // quick decay
-  [0.20,   2,    1,     0.08],  // brief sustain
-  [0.00,   0,    0,     0.05]
+  [0.00,   6,    12,     0.00],
+  [0.90,   13,    16,    0.02],  // bite
+  [0.40,   10,    14,    0.04],  // quick decay
+  [0.20,   8,    12,     0.08],  // brief sustain
+  [0.00,   6,    12,     0.05]
   ],
   [                     // amp envelope 3 ; SPICCATO
-  [0.00,   0,    0,     0.00],
-  [0.95,   9,    5,     0.015], // crisp ping from bow bounce
-  [0.35,   5,    2,     0.03],
-  [0.15,   2,    1,     0.05],
-  [0.00,   0,    0,     0.04]
+  [0.00,   6,    12,     0.00],
+  [0.8,   15,    17,     0.02],  // very sharp bite from the bow impact
+  [0.45,  10,    6,      0.02],   // immediate fall as bow leaves the string
+  [0.00,   6,    0,      0.50],    // long ringing body resonance (release)
+  [0.00,   0,    0,      0.05],    
   ],
   [                     // amp envelope 4 ; SFORZANDO
-  [0.00,    0,     0,      0.00],
-  [1.10,   10,     6,      0.02],   // strong accent
-  [0.70,    5,     3,      0.06],   // fast fall
-  [0.55,    3,     2,      0.30],   // sustain at moderate intensity
+  [0.00,    6,    12,      0.00],
+  [0.9,    16,     18,     0.02],   // strong accent
+  [0.70,   11,     15,     0.06],   // fast fall
+  [0.55,    9,     15,     0.30],   // sustain at moderate intensity
   [0.00,    0,     0,      0.25]
   ],
   [                     // amp envelope 5 ; SLURRED
-  [0.00,   0,   0,    0.00],
-  [0.70,   3,   1,    0.18],   // soft bow change
-  [0.55,   2,   1,    0.12],
-  [0.50,   1,   1,    0.50],   // very stable sustain
-  [0.00,   0,   0,    0.25]
+  [0.00,   6,    9,     0.00],
+  [0.70,   9,    13,    0.30],   // soft bow change
+  [0.55,   8,    12,    0.12],
+  [0.50,   7,    9,     0.50],   // very stable sustain
+  [0.00,   6,    9,     0.25]
   ]
 ];
-  
+
   let maxGain = 0.2;
   let vibAmount = 0;
   document.querySelector("#vibratoAmount").addEventListener("input", (event)=>{
@@ -65,12 +72,13 @@ const envArray = [     // array of all amplitude envelopes
     vibAmount = Number(event.target.value);
   });
 
+
 const midi = new MIDIengine();
 
 // paramaters note and velocity for starting a new note
 midi.onNoteOn = (note, velocity) => {
     myNotes[note] = new Synth(ctx, note, velocity, maxGain, envArray[articNum], vibAmount);   // passing note and velocity as parameters for a new synth note... also using the ADSR array as a parameter
-    myNotes[note].ampEnv.connect(master);      // connect the envelope (gain node of Synth) to the master
+    myNotes[note].ampEnv.connect(lpFilter);      // connect the envelope (gain node of Synth) to the master
     myNotes[note].start(note, velocity);
     console.log("start");
 }
@@ -83,13 +91,18 @@ midi.onNoteOff = (note) => {
 const startButton = document.querySelector("#startButton");
 startButton.addEventListener('click', () => {
   ctx.resume();
-  console.log("click!");
+  console.log("resumed!");
 })
 const stopButton = document.querySelector("#stopButton");
 stopButton.addEventListener('click', () => {
   ctx.suspend();
-  console.log("clickz");
-})
+  console.log("suspend");
+  for (let i = 0; i < myNotes.length-1; i++) {
+  if (myNotes[i]) {
+    myNotes[i].osc.stop();
+    }
+  }
+});
 
 
 // --- --- --- also master gain slider
@@ -147,14 +160,23 @@ artic5.addEventListener('click', () => {
 dynamic1.addEventListener('click', () => {
   dynamicNum = 0;
   console.log(dynamicNum);
+  maxGain = 0.1;
+  lpFilter.frequency.value = 2500;
+  lpFilter.gain.value = -6;
 });
 
 dynamic2.addEventListener('click', () => {
   dynamicNum = 1;
   console.log(dynamicNum);
+  maxGain = 0.4;
+  lpFilter.frequency.value = 3300;
+  lpFilter.gain.value = -6;
 });
 
 dynamic3.addEventListener('click', () => {
   dynamicNum = 2;
   console.log(dynamicNum);
+  maxGain = 0.9;
+  lpFilter.frequency.value = 4000;
+  lpFilter.gain.value = 3;
 });
